02b6a529b21b2ebffe2f84531d94a2890f3b6add
snipestorm <adam_hanford@hotmail.co.uk>
2022-09-08 01:32:52 +0100

    More Artifacts

---
+++ src/main/java/net/adam/dungeonmod/fluid/SoulLavaFluid.java
@@ -0,0 +1,227 @@
+package net.adam.dungeonmod.fluid;
+import net.minecraft.block.*;
+import net.minecraft.block.entity.BlockEntity;
+import net.minecraft.fluid.FlowableFluid;
+import net.minecraft.fluid.Fluid;
+import net.minecraft.fluid.FluidState;
+import net.minecraft.item.Item;
+import net.minecraft.particle.ParticleTypes;
+import net.minecraft.sound.SoundCategory;
+import net.minecraft.sound.SoundEvent;
+import net.minecraft.sound.SoundEvents;
+import net.minecraft.state.StateManager;
+import net.minecraft.state.property.Properties;
+import net.minecraft.tag.FluidTags;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
+import net.minecraft.util.math.random.Random;
+import net.minecraft.world.*;
+
+import java.util.Optional;
+
+public abstract class SoulLavaFluid extends FlowableFluid {
+    public static final float field_31729 = 0.44444445F;
+    @Override
+    protected boolean isInfinite() {
+        return false;
+    }
+
+    @Override
+    protected void beforeBreakingBlock(WorldAccess world, BlockPos pos, BlockState state) {
+        final BlockEntity blockEntity = state.hasBlockEntity() ? world.getBlockEntity(pos) : null;
+        Block.dropStacks(state, world, pos, blockEntity);
+    }
+
+    @Override
+    public int getFlowSpeed(WorldView world) {
+        return world.getDimension().ultrawarm() ? 4 : 2;
+    }
+
+
+    @Override
+    protected int getLevelDecreasePerBlock(WorldView world) {
+        return 1;
+    }
+
+    @Override
+    public boolean matchesType(Fluid fluid) {
+        return fluid == getStill() || fluid == getFlowing();
+    }
+
+    @Override
+    public int getLevel(FluidState state) {
+        return 0;
+    }
+
+    @Override
+    public int getTickRate(WorldView world) {
+        return world.getDimension().ultrawarm() ? 10 : 30;
+    }
+
+
+    @Override
+    protected float getBlastResistance() {
+        return 100f;
+    }
+
+    @Override
+    protected boolean canBeReplacedWith(FluidState state, BlockView world, BlockPos pos, Fluid fluid, Direction direction) {
+        return false;
+    }
+
+    @Override
+    public void randomDisplayTick(World world, BlockPos pos, FluidState state, Random random) {
+        BlockPos blockPos = pos.up();
+        if (world.getBlockState(blockPos).isAir() && !world.getBlockState(blockPos).isOpaqueFullCube(world, blockPos)) {
+            if (random.nextInt(100) == 0) {
+                double d = (double)pos.getX() + random.nextDouble();
+                double e = (double)pos.getY() + 1.0;
+                double f = (double)pos.getZ() + random.nextDouble();
+                world.addParticle(ParticleTypes.LAVA, d, e, f, 0.0, 0.0, 0.0);
+                world.playSound(d, e, f, SoundEvents.BLOCK_LAVA_POP, SoundCategory.BLOCKS, 0.2F + random.nextFloat() * 0.2F, 0.9F + random.nextFloat() * 0.15F, false);
+            }
+
+            if (random.nextInt(200) == 0) {
+                world.playSound((double)pos.getX(), (double)pos.getY(), (double)pos.getZ(), SoundEvents.BLOCK_LAVA_AMBIENT, SoundCategory.BLOCKS, 0.2F + random.nextFloat() * 0.2F, 0.9F + random.nextFloat() * 0.15F, false);
+            }
+        }
+
+    }
+    public void onRandomTick(World world, BlockPos pos, FluidState state, Random random) {
+        if (world.getGameRules().getBoolean(GameRules.DO_FIRE_TICK)) {
+            int i = random.nextInt(3);
+            if (i > 0) {
+                BlockPos blockPos = pos;
+
+                for(int j = 0; j < i; ++j) {
+                    blockPos = blockPos.add(random.nextInt(3) - 1, 1, random.nextInt(3) - 1);
+                    if (!world.canSetBlock(blockPos)) {
+                        return;
+                    }
+
+                    BlockState blockState = world.getBlockState(blockPos);
+                    if (blockState.isAir()) {
+                        if (this.canLightFire(world, blockPos)) {
+                            world.setBlockState(blockPos, AbstractFireBlock.getState(world, blockPos));
+                            return;
+                        }
+                    } else if (blockState.getMaterial().blocksMovement()) {
+                        return;
+                    }
+                }
+            } else {
+                for(int k = 0; k < 3; ++k) {
+                    BlockPos blockPos2 = pos.add(random.nextInt(3) - 1, 0, random.nextInt(3) - 1);
+                    if (!world.canSetBlock(blockPos2)) {
+                        return;
+                    }
+
+                    if (world.isAir(blockPos2.up()) && this.hasBurnableBlock(world, blockPos2)) {
+                        world.setBlockState(blockPos2.up(), AbstractFireBlock.getState(world, blockPos2));
+                    }
+                }
+            }
+
+        }
+    }
+
+    private boolean canLightFire(WorldView world, BlockPos pos) {
+        Direction[] var3 = Direction.values();
+        int var4 = var3.length;
+
+        for(int var5 = 0; var5 < var4; ++var5) {
+            Direction direction = var3[var5];
+            if (this.hasBurnableBlock(world, pos.offset(direction))) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    private boolean hasBurnableBlock(WorldView world, BlockPos pos) {
+        return pos.getY() >= world.getBottomY() && pos.getY() < world.getTopY() && !world.isChunkLoaded(pos) ? false : world.getBlockState(pos).getMaterial().isBurnable();
+    }
+
+
+    private void playExtinguishEvent(WorldAccess world, BlockPos pos) {
+        world.syncWorldEvent(1501, pos, 0);
+    }
+
+    @Override
+    protected void flow(WorldAccess world, BlockPos pos, BlockState state, Direction direction, FluidState fluidState) {
+        if (direction == Direction.DOWN) {
+            FluidState fluidState2 = world.getFluidState(pos);
+        if (this.isIn(FluidTags.LAVA) && fluidState2.isIn(FluidTags.LAVA)) {
+                if (state.getBlock() instanceof FluidBlock) {
+                    world.setBlockState(pos, Blocks.STONE.getDefaultState(), 3);
+                }
+
+                this.playExtinguishEvent(world, pos);
+                return;
+            }
+        }
+
+        super.flow(world, pos, state, direction, fluidState);
+    }
+
+    @Override
+    public Optional<SoundEvent> getBucketFillSound() {
+        return Optional.of(SoundEvents.ITEM_BUCKET_FILL);
+    }
+
+    @Override
+    public Fluid getStill() {
+        return ModFluids.STILL_SOUL_LAVA;
+    }
+
+    @Override
+    public Fluid getFlowing() {
+        return ModFluids.FLOWING_SOUL_LAVA;
+    }
+
+    @Override
+    public Item getBucketItem() {
+        return ModFluids.SOUL_LAVA_BUCKET;
+    }
+
+    @Override
+    protected BlockState toBlockState(FluidState state) {
+        return ModFluids.SOUL_LAVA_BLOCK.getDefaultState().with(FluidBlock.LEVEL, getBlockStateLevel(state));
+    }
+
+    @Override
+    public boolean isStill(FluidState state) {
+        return false;
+    }
+
+    public static class Flowing extends SoulLavaFluid {
+        @Override
+        protected void appendProperties(StateManager.Builder<Fluid, FluidState> builder) {
+            super.appendProperties(builder);
+            builder.add(LEVEL);
+        }
+
+        @Override
+        public int getLevel(FluidState state) {
+            return state.get(LEVEL);
+        }
+
+        @Override
+        public boolean isStill(FluidState state) {
+            return false;
+        }
+    }
+
+    public static class Still extends SoulLavaFluid {
+        @Override
+        public int getLevel(FluidState state) {
+            return 8;
+        }
+
+        @Override
+        public boolean isStill(FluidState state) {
+            return true;
+        }
+    }
+}
---
+++ src/main/java/net/adam/dungeonmod/item/custom/AstralArtifact.java
@@ -0,0 +1,35 @@
+package net.adam.dungeonmod.item.custom;
+
+import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.client.item.TooltipContext;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.text.Text;
+import net.minecraft.util.Formatting;
+import net.minecraft.world.World;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
+
+public class AstralArtifact extends Item {
+
+    public AstralArtifact(Settings settings) {
+        super(settings);
+    }
+
+
+
+
+    @Override
+    public void appendTooltip(ItemStack stack, @Nullable World world, List<Text> tooltip, TooltipContext context) {
+        if (Screen.hasShiftDown()) {
+            tooltip.add(Text.literal("Add Me To Your Board,").formatted(Formatting.GOLD));
+            tooltip.add(Text.literal("Trade Me Back").formatted(Formatting.GOLD));
+            tooltip.add(Text.literal("To The Dungeon,").formatted(Formatting.GOLD));
+            tooltip.add(Text.literal("Or Transmute Me...").formatted(Formatting.GOLD));
+            super.appendTooltip(stack, world, tooltip, context);
+        } else tooltip.add(Text.literal("Part Of The Astral Set,").formatted(Formatting.AQUA));
+        tooltip.add(Text.literal("Press Shift For More Info.").formatted(Formatting.AQUA));
+        super.appendTooltip(stack, world, tooltip, context);
+    }
+}
